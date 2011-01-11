bbf087176e2b59b6568a1247c782b5d01e575565
pablodip <pablodip@gmail.com>
2011-01-11 10:52:01 +0100

    fixed warning in phpunit

--- tests/Mondongo/Tests/Type/TestCase.php
+++ tests/Mondongo/Tests/Type/TestCase.php
@@ -21,7 +21,7 @@
 
 namespace Mondongo\Tests\Type;
 
-class TestCase extends \PHPUnit_Framework_TestCase
+abstract class TestCase extends \PHPUnit_Framework_TestCase
 {
     protected function getTypeFunction($string)
     {
